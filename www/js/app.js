a5c8239d6270380edf7fc75553430635c4541b34
Michael Bigelow <procureeminence@gmail.com>
2020-01-13 17:53:27 -0500

    Removed Debug URL & Added SSL

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,13 @@
 // OpenWeatherMap API
 const personalKey = '4de8f23828879dec98d1f230e00b3bd4';
-const baseURL1 = 'api.openweathermap.org/data/2.5/weather?zip=';
+const baseURL1 = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 const baseURL2 = '&appid=';
 
 const createWeatherURL = ( zipCode ) => {
-    //return baseURL1 + zipCode + baseURL2 + personalKey;
-    return 'https://samples.openweathermap.org/data/2.5/weather?zip=13659,us&appid=b6907d289e10d714a6e88b30761fae22'; // Debug API URL
-};
 
-/* Function called by event listener */
+	return baseURL1 + zipCode + baseURL2 + personalKey;
+
+};
 
 /* Function to GET Web API Data*/
 
